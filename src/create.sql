09e3a7d39fb6a5d770175128f27b5a56aa0d31fa
renanmirandae <renanmirandae@gmail.com>
2022-09-01 12:04:46 -0300

    Inserindo querys para criação do banco de dados

--- src/create.sql
+++ src/create.sql
@@ -1,1 +1,39 @@
-create table if not exists
+create table if not exists dentista(
+id int primary key auto_increment,
+nome varchar(30) not null,
+sobrenome varchar(30) not null,
+matricula varchar(15) not null unique
+);
+
+create table if not exists paciente(
+id int primary key auto_increment,
+nome varchar(30) not null,
+sobrenome varchar(30) not null,
+rg varchar(15) not null unique,
+dataAlta Date
+);
+
+create table if not exists consulta(
+id int primary key auto_increment,
+data Date,
+hora Time,
+idDentista int,
+foreign key(idDentista)
+references dentista(id),
+idPaciente int,
+foreign key(idPaciente)
+references paciente(id)
+);
+
+create table if not exists endereco(
+id int primary key auto_increment,
+logradouro varchar(10) not null,
+endereco varchar(100) not null,
+numero varchar(5) not null,
+cidade varchar(20) not null,
+estado char(2) not null,
+cep char(8) not null,
+idPaciente int,
+foreign key (idPaciente)
+references paciente(id)
+);
